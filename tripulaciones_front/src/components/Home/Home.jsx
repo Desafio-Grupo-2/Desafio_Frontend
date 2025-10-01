386181952f0722fa5840b67bd8d9edfc782b5682
GuilleG02 <113667687+GuilleG02@users.noreply.github.com>
2025-10-01 11:28:10 +0200

    Mejora del Responsive y eliminacion del MN (#26) (#28)

--- tripulaciones_front/src/components/Home/Home.jsx
+++ tripulaciones_front/src/components/Home/Home.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import Map from "./Map";
-import { MapThemeProvider } from "../../contexts/MapThemeContext";
 import "../../assets/styles/components/home/home.scss";
-import "../../styles/themes/mapDarkMode.scss";
 
 const Home = () => {
   return (
-    <MapThemeProvider>
-      <div>
-        <Map />
-      </div>
-    </MapThemeProvider>
+    <div>
+      <Map />
+    </div>
   );
 };
 
